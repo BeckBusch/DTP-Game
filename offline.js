6684abecaa04a039428f085bd9c35d3ebc4acf1f
Beck Busch <beckbusch5@gmail.com>
2017-08-04 12:06:01 +1200

    update

--- offline.js
+++ offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1501198113,
+	"version": 1501805095,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -17,14 +17,25 @@
 		"images/spawner-sheet1.png",
 		"images/flying-sheet0.png",
 		"images/cloud-sheet0.png",
-		"images/cloudspawner-sheet0.png",
 		"images/bullet-sheet0.png",
+		"images/tiledbackground2.png",
+		"images/sprite-sheet0.png",
+		"images/tiledbackground3.png",
+		"images/particles.png",
+		"images/particles2.png",
+		"images/crackedboat-sheet0.png",
+		"images/crackedboat-sheet1.png",
+		"images/tiledbackground4.png",
+		"images/platform-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/spritefont.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
 		"icon-128.png",
 		"icon-256.png",
 		"loading-logo.png",
+		"penumbra.png",
 		"pathfind.js"
 	]
 }
